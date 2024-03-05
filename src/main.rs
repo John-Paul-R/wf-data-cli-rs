8db3303f1083ea998bfee1911e472e8d32caa94b
John Paul Rutigliano <johnpaul122jpr@gmail.com>
2024-03-04 23:07:26 -0500

    add pretty format

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,28 @@ use std::io;
 use serde_json::Result;
 use std::env;
 use std::collections::HashSet;
+use term_size::dimensions_stdout;
+
+#[derive(Debug, Deserialize, Serialize)]
+struct Reward {
+    rarity: String,
+    chance: f64,
+    item: RewardItem,
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+struct RewardItem {
+    name: String,
+    uniqueName: String,
+    #[serde(default)]
+    warframeMarket: Option<WarframeMarket>,
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+struct WarframeMarket {
+    id: String,
+    urlName: String,
+}
 
 #[derive(Debug, Deserialize, Serialize)]
 struct Patchlog {
@@ -41,7 +63,7 @@ struct Item {
     uniqueName: String,
     description: Option<String>,
     #[serde(rename = "type")]
-    type_: Option<String>,
+    type_: String,
     tradable: bool,
     category: Option<String>,
     productCategory: Option<String>,
@@ -49,6 +71,7 @@ struct Item {
     components: Option<Vec<Component>>,
     introduced: Option<Introduced>,
     estimatedVaultDate: Option<String>,
+    rewards: Option<Vec<Reward>>, // Include the rewards property
 }
 
 #[derive(Debug, PartialEq)]
@@ -100,28 +123,91 @@ impl Item {
     }
 }
 
+fn wrap_text(text: &str, prefix: &str, max_width: usize, indent_after_first: usize) -> Vec<String> {
+    let mut lines = vec![];
+    let mut current_line = String::from(prefix);
+    let prefix_width = unicode_width::UnicodeWidthStr::width(prefix);
+
+    for word in text.split_whitespace() {
+        let word_width = unicode_width::UnicodeWidthStr::width(word);
+        if current_line.is_empty() {
+            current_line.push_str(word);
+        } else if unicode_width::UnicodeWidthStr::width(format!("{} {} {}", prefix, &current_line, word).as_str()) <= max_width {
+            current_line.push(' ');
+            current_line.push_str(word);
+        } else {
+            lines.push(current_line.clone());
+            current_line = " ".repeat(indent_after_first);
+            current_line.push_str(word);
+        }
+    }
+    lines.push(current_line);
+
+    lines
+}
+
 fn log_items(items: Vec<Item>, output_format: OutputFormat, has_relic_arg: bool) {
     let mut unique_items: HashSet<String> = HashSet::new();
+
+
+    let (term_width, _) = dimensions_stdout().unwrap_or((80, 24)); // Default width: 80, height: 24
 
     for item in items {
         match output_format {
             OutputFormat::Default => {
-                println!("Name: {}", item.name);
-                println!("UniqueName: {}", item.uniqueName);
-                println!("Description: {}", item.description.unwrap_or("NOT PRESENT".to_string()));
-                println!("Type: {}", item.type_.unwrap_or("NOT PRESENT".to_string()));
-                println!("Tradable: {}", item.tradable);
-                println!("Category: {}", item.category.unwrap_or("NOT PRESENT".to_string()));
-                println!("Product Category: {}", item.productCategory.unwrap_or("NOT PRESENT".to_string()));
-                println!(
-                    "Introduced Date: {}",
-                    item.introduced
-                        .as_ref()
-                        .map_or_else(|| "NOT PRESENT".to_string(), |v| v.date.clone())
-                );
-                println!("Estimated Vault Date: {}", item.estimatedVaultDate.unwrap_or("NOT PRESENT".to_string()));
-                println!("---");
-            },
+                // Calculate border width
+                let border_width = term_width - 2; // Subtract 2 for the borders
+                
+                println!("┌{}┐", "─".repeat(border_width));
+                println!("│ Name: {}", item.name);
+                println!("│ UniqueName: {}", item.uniqueName);
+                if let Some(description) = &item.description {
+                    let desc_lines = wrap_text(description, "Description:", border_width, 2);
+                    for line in desc_lines {
+                        println!("│ {}", line);
+                    }
+                }
+                println!("│ Type: {}", item.type_);
+                println!("│ Tradable: {}", item.tradable);
+                if let Some(category) = &item.category {
+                    println!("│ Category: {}", category);
+                }
+                if let Some(product_category) = &item.productCategory {
+                    println!("│ Product Category: {}", product_category);
+                }
+                if let Some(introduced) = &item.introduced {
+                    println!("│ Introduced Date: {}", introduced.date);
+                }
+                if let Some(vault_date) = &item.estimatedVaultDate {
+                    println!("│ Estimated Vault Date: {}", vault_date);
+                }
+                if let Some(rewards) = &item.rewards {
+                    for reward in rewards {
+                        println!("│   - {}", reward.item.name);
+                    }
+                }
+                println!("└{}┘", "─".repeat(border_width));
+            }
+            // OutputFormat::Default => {
+            //     println!("Name: {}", item.name);
+            //     println!("UniqueName: {}", item.uniqueName);
+            //     println!("Description: {:?}", item.description);
+            //     println!("Type: {}", item.type_);
+            //     println!("Tradable: {}", item.tradable);
+            //     println!("Category: {:?}", item.category);
+            //     println!("Product Category: {:?}", item.productCategory);
+            //     println!("Introduced Date: {:?}", item.introduced.as_ref().map(|v| &v.date));
+            //     println!("Estimated Vault Date: {:?}", item.estimatedVaultDate);
+
+            //     // Print rewards if available
+            //     if let Some(rewards) = &item.rewards {
+            //         for reward in rewards {
+            //             println!("  - {}", reward.item.name);
+            //         }
+            //     }
+
+            //     println!("---");
+            // }
             OutputFormat::Search => {
                 if has_relic_arg {
                     let short_name = item.get_relic_short_name();
@@ -129,18 +215,17 @@ fn log_items(items: Vec<Item>, output_format: OutputFormat, has_relic_arg: bool)
                         println!("{}", short_name);
                     }
                 } else {
-                    println!("{} ({})", item.name, item.uniqueName);
+                    println!("{}", item.name);
                 }
             }
         }
     }
 }
 
-
 fn filter_items_by_relic_type(items: Vec<Item>, relic_type: Option<RelicType>) -> Vec<Item> {
     items.into_iter().filter(|item| {
         // Filter logic: check if the item's type is "relic"
-        let is_relic = item.type_.as_deref() == Some("Relic");
+        let is_relic = &item.type_ == "Relic";
 
         // println!("type: {:?}, is_relic: {:?}", item.type_, is_relic);
         // If a relic type was provided, additionally check if the item's uniqueName starts with the string form of the relic type
@@ -170,6 +255,7 @@ fn filter_items_by_search_term(items: Vec<Item>, search_term: Option<String>) ->
 }
 // get_wf_items() { cat ./data.json | ./target/release/wf_api_quick --log-items "$@" }
 // search_relics () { get_wf_items --search "$(get_wf_items --fmt:search --relic | fzf)" }     
+// search_wf_items () { get_wf_items --search "$(get_wf_items --fmt:search | fzf)" }     
 
 fn main() -> Result<()> {
     let args: Vec<String> = env::args().collect();
